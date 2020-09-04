3300642cb23b62a88a7f0fd3d66477b5d152abe1
nesadrian <adrian.nesvik@lyse.net>
2020-09-04 23:25:55 +0200

    Add scoket.io instance + connection listening

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const app = require('express')();
+const http = require('http').createServer(app);
+const socketIO = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+socketIO.on('connection', (socket) => {
+    console.log("User connected")
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+})
 
 http.listen(3000, () => {
 });
